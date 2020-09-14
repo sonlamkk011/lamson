9f2dc2019feab542bc893c3b9fdb09299483d67e
sonlamkk011 <71081269+sonlamkk011@users.noreply.github.com>
2020-09-14 12:59:04 +0700

    bai 2

---
+++ bài 2 t7 code.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int a,b;
+	printf("nhap a=");
+	scanf("%d",&a);
+	printf("nhap b=");
+	scanf("%d",&b);
+	if(a>=b){
+	    printf("a*b=%d",a*b);
+	}else{
+		printf("a/b=%d",a/b);
+	}
+	
+}
